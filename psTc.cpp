8b3533bd17f5e93aa722110d03cbc082e93407b8
A7med-M <ahmed.mahmoud0143@gmail.com>
2026-05-17 18:48:13 +0300

    CP templates

---
+++ psTc.cpp
@@ -0,0 +1,32 @@
+/**
+ *  ❤️ وَتَوَكَّلْ عَلَى اللَّهِ ۚ وَكَفَىٰ بِاللَّهِ وَكِيلًا ❤️
+ *    Free Palestine | فلسطين حرة 
+**/
+ 
+/**
+ *  AHMED MAHMOUD
+**/
+ 
+
+#include<bits/stdc++.h>
+
+#define int long long
+
+using namespace std;
+
+void yalla_binaaaaaa(int t) {
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for(int t = 1; t <= tc; t++) {
+        yalla_binaaaaaa(t);
+    }
+
+    return 0;
+}
+
